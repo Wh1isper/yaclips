1173aab8182ae73a2cd63a4c8e33d16d0722fb55
Wh1isper <9573586@qq.com>
2024-01-28 21:35:57 +0800

    Adding CI

--- generate_proto.sh
+++ generate_proto.sh
@@ -8,5 +8,6 @@ python -m grpc_tools.protoc -I./proto --pyi_out=./yaclips/proto --python_out=./y
 2to3 -n -w ./yaclips/proto/*_pb2*
 
 echo "Generating client package's proto..."
-python -m grpc_tools.protoc -I./proto --pyi_out=./yaclips/client/proto --python_out=./yaclips/client/proto --grpc_python_out=./yaclips/client/proto ./proto/yaclips.proto
+python -m grpc_tools.protoc -I./proto --pyi_out=./client/yaclips_client/proto --python_out=./client/yaclips_client/proto --grpc_python_out=./client/yaclips_client/proto ./proto/yaclips.proto
+2to3 -n -w ./client/yaclips_client/proto/*_pb2*
 
